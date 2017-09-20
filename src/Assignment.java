fca3d379e1f7b9891f785c88587a5993bdeee80f
bitnahian <nhas9102@uni.sydney.edu.au>
2017-09-20 22:05:35 +1000

    Cleaned up code and added comments

--- src/Assignment.java
+++ src/Assignment.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Assignment implements SubmissionHistory {
 
 	private HashMap<String , MyTreeMap> bigMap;
-	private HashMap<String, Integer> bestGrades; // This needs to be sorted
+	private HashMap<String, Integer> bestGrades;
 
 	/**
 	 * Default constructor
@@ -11,118 +11,157 @@ public class Assignment implements SubmissionHistory {
 	public Assignment() {
 
 	    bigMap = new HashMap<>();
-	    bestGrades = new HashMap<>(); // For sorting in descending order
-		// TODO initialise your data structures
+	    bestGrades = new HashMap<>();
 	}
 
+    /**
+     *
+     * @param unikey
+     *            The student to filter on
+     * @return Submission made most recently by that student, or null if the
+     *         student has made no submissions
+     * @throws IllegalArgumentException
+     *            If the argument is null or is not a String object
+     */
 	@Override
 	public Integer getBestGrade(String unikey) throws IllegalArgumentException {
 	    if(!(unikey instanceof String ))
 	        throw new IllegalArgumentException();
-	    if(bigMap.containsKey(unikey))
-	        return bigMap.get(unikey).getBestGrade(); // O(1) + O(log n) = O(log n)
-        return null;
-		// TODO Implement this, ideally in better than O(n)
-
+	    if(bigMap.containsKey(unikey))                  // If student has entries in bigMap
+	        return bigMap.get(unikey).getBestGrade();   // Return best grade
+        return null;                                    // Else return null
 	}
 
+    /**
+     * \
+     * @param unikey
+     *            The student to filter on
+     * @return Submission made most recently by that student, or null if the
+     *         student has made no submissions
+     * @throws IllegalArgumentException
+     *            If the argument is null or is not a String object
+     */
 	@Override
 	public Submission getSubmissionFinal(String unikey) throws IllegalArgumentException{
-		// TODO Implement this, ideally in better than O(n)
         if(!(unikey instanceof String ))
             throw new IllegalArgumentException();
-        if(bigMap.containsKey(unikey))
-		    return bigMap.get(unikey).getMostRecentSubmission();
-        return null;
+        if(bigMap.containsKey(unikey))                          // If student has entries in bigMap
+		    return bigMap.get(unikey).getMostRecentSubmission();// Return the most recent submission
+        return null;                                            // Else return null
 	}
 
+    /**
+     *
+     * @param unikey
+     *            The student to filter on
+     * @param deadline
+     *            The deadline after which no submissions are considered
+     * @return Submission made most recently by that student, or null if the
+     *         student has made no submissions
+     * @throws IllegalArgumentException
+     *            If the argument is null
+     */
 	@Override
 	public Submission getSubmissionBefore(String unikey, Date deadline) throws IllegalArgumentException{
-		// TODO Implement this, ideally in better than O(n)
         if(!(unikey instanceof String) || !(deadline instanceof Date))
             throw new IllegalArgumentException();
-
-		return bigMap.get(unikey).getSubmissionPriorToTime(deadline);
+		return bigMap.get(unikey).getSubmissionPriorToTime(deadline);   // Function returns appropriately
 	}
 
+    /**
+     *
+     * @param unikey
+     * @param timestamp
+     * @param grade
+     * @return the Submission object that was created
+     * @throws IllegalArgumentException
+     *         If the arguments are null
+     */
 	@Override
 	public Submission add(String unikey, Date timestamp, Integer grade) throws IllegalArgumentException{
-		// TODO Implement this, ideally in better than O(n)
-        // Get prev best grade
         if(!(unikey instanceof String) || !(timestamp instanceof Date) || !(grade instanceof Integer))
             throw new IllegalArgumentException();
 
-
-        if(!bigMap.containsKey(unikey))
+        if(!bigMap.containsKey(unikey))             // If the student does not already have an entry in bigMap
         {
-            MyTreeMap myTreeMap = new MyTreeMap();
+            MyTreeMap myTreeMap = new MyTreeMap();  // Make new MyTreeMap
             Submission submission = new MyTreeMap.MyEntry(unikey, timestamp, grade);
-            myTreeMap.addSubmission(submission);
-            bigMap.put(unikey, myTreeMap);
-            bestGrades.put(unikey, grade);
+            myTreeMap.addSubmission(submission);    // And add new submission to MyTreeMap
+            bigMap.put(unikey, myTreeMap);          // Make a new entry for student with the new MyTreeMap
+            bestGrades.put(unikey, grade);          // Make a new entry for best grades for that student
             return submission;
         }
         else
         {
-            // If student already exists
-            MyTreeMap treeMap = bigMap.get(unikey);
-            int prevBestGrade = treeMap.getBestGrade(); // O(log n)
+            MyTreeMap treeMap = bigMap.get(unikey);     // If student already exists in the bigMap
+            int prevBestGrade = treeMap.getBestGrade(); // Get the student's current best grade - O(log n)
             Submission submission = new MyTreeMap.MyEntry(unikey, timestamp, grade);
-            treeMap.addSubmission(submission);
-            // If this grade has become the best grade
-            if (grade > prevBestGrade)
-                bestGrades.put(unikey, grade);
+            treeMap.addSubmission(submission);          // Add a new submission to MyTreeMap
+
+            if (grade > prevBestGrade)                  // If this grade is greater than the current best grade
+                bestGrades.put(unikey, grade);          // Change best grade to new grade
             return submission;
         }
 	}
 
+    /**
+     *
+     * @param submission
+     *            The Submission to remove
+     * @throws IllegalArgumentException
+     *         If the argument is null or is not a Submission
+     */
 	@Override
 	public void remove(Submission submission) throws IllegalArgumentException{
-		// TODO Implement this, ideally in better than O
         if(!(submission instanceof Submission))
             throw new IllegalArgumentException();
-        if(bigMap.size() == 0)
+        if(bigMap.size() == 0)          // If the bigMap doesn't contain entries then return
             return;
-        MyTreeMap treeMap= bigMap.get(submission.getUnikey()); // O(1)
-        int prevBestGrade = treeMap.getBestGrade(); //O(log n)
-		if(treeMap.removeSubmission(submission) && // O(log n)
-                prevBestGrade == submission.getGrade())
-        {
-            bestGrades.put(submission.getUnikey(), treeMap.getBestGrade()); // O(log n) + O(1)
+        MyTreeMap treeMap= bigMap.get(submission.getUnikey());  // Get the treeMap - O(1)
+        int prevBestGrade = treeMap.getBestGrade();             // Get the current best grade - O(log n)
+		if(treeMap.removeSubmission(submission) &&              // If removeSubmission returns true
+                prevBestGrade == submission.getGrade())         // And current best grade is the same as removed grade
+        {                                                       // Put the next best grade in bestGrades - O(log n) + O(1)
+            bestGrades.put(submission.getUnikey(), treeMap.getBestGrade());
         }
 	}
 
+    /**
+     *
+     * @return The List of top students
+     */
 	@Override
 	public List<String> listTopStudents() {
-		// TODO Implement this, ideally in better than O(n)
-		// (you may ignore the length of the list in the analysis)
         ArrayList<String> topStudents = new ArrayList<>();
         int max = 0;
-        for(Map.Entry<String, Integer> entry : bestGrades.entrySet())
-        // Iteration over HashMap STRICTLY SMALLER THAN n AS SPECIFIED < O(n)
-        {
-            if(entry.getValue() > max) {
-                max = entry.getValue();
-                topStudents.clear();
-                topStudents.add(entry.getKey());
+        for(Map.Entry<String, Integer> entry : bestGrades.entrySet()) // Iteration over HashMap STRICTLY SMALLER THAN n AS SPECIFIED
+        {                                           // As we can assume that (size of students is sublinear in bestGrades) < n
+            if(entry.getValue() > max) {            // If the value is greater than current max
+                max = entry.getValue();             // Set max to current value
+                topStudents.clear();                // Clear the ArrayList
+                topStudents.add(entry.getKey());    // Add back to ArrayList
             }
             else if(entry.getValue() == max)
             {
-                topStudents.add(entry.getKey());
-            }
+                topStudents.add(entry.getKey());    // Else if max and value is equal
+            }                                       // Add to ArrayList
 
         }
-        // Get grades from HashMap, which is significantly smaller than
 		return topStudents;
 	}
 
+    /**
+     *
+     * @return The list of Regressed Students
+     */
 	@Override
 	public List<String> listRegressions() {
-		// TODO Implement this, ideally in better than O(n^2)
 		ArrayList<String> regressedStudents = new ArrayList<>();
-        for(Map.Entry<String, MyTreeMap> entry : bigMap.entrySet())
+        for(Map.Entry<String, MyTreeMap> entry : bigMap.entrySet()) // Iterate through every student
         {
-            if(entry.getValue().getBestGrade() > entry.getValue().getMostRecentSubmission().getGrade())
+            Integer bestGrade = entry.getValue().getBestGrade();                                  // Get student's best grade
+            Integer mostRecentSubmission = entry.getValue().getMostRecentSubmission().getGrade(); // Get student's latest submission
+            if(bestGrade > mostRecentSubmission)
                 regressedStudents.add(entry.getKey());
         }
         return regressedStudents;
